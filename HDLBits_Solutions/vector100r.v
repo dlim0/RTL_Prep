6f6f8292e3adb4f9cac1317f2a46c065dbd5ac5d
Daniel Lim <dsl79@case.edu>
2026-07-30 00:11:15 -0400

    Create vector100r.v

---
+++ HDLBits_Solutions/vector100r.v
@@ -0,0 +1,13 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    integer i;
+	always_comb
+        begin
+            for(i=0; i<100; i=i+1) begin
+            	out[i]=in[99-i];
+    		    end
+       	end
+endmodule
+//implemented based on the solution learned from vectorr.v
